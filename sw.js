bc6111ac2f4f45c7243f69d2fd792025f3d2619e
refaelzakai-sys <refaelzakai@gmail.com>
2026-04-27 23:49:06 +0300

    Implement install and fetch event listeners in sw.js
    Add service worker event listeners for install and fetch.

--- sw.js
+++ sw.js
@@ -1,1 +1,8 @@
-self.addEventListener('fetch', () => {});
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', (event) => {
+  // נדרש כדי שהדפדפן יזהה את האתר כ-PWA
+});
+
